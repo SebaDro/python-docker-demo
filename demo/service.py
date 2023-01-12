fbfdb4d0972582451d53bfca5d2bfa053a4c57e7
Sebastian Drost <s.drost@52north.org>
2023-01-12 14:30:52 +0100

    Add configurable greetings phrase

--- demo/service.py
+++ demo/service.py
@@ -1,7 +1,9 @@
-import json
+import os
 
 from flask import request, Response, abort
 from demo import app
+
+default_greeting = "Hello"
 
 
 @app.route("/greeting", methods=["GET"])
@@ -10,5 +12,8 @@ def greeting():
     # If no query parameter have been specified, use a default value.
     name = request.args.get("name", default="KomMonitor", type=str)
 
+    # Read out the CUSTOM_GREETINGS environment variable. If not set, use a default greeting.
+    greetings_text = os.environ.get("CUSTOM_GREETING", default_greeting)
+
     # Return the Response
-    return Response(f"Hello {name}", mimetype='text/plain')
+    return Response(f"{greetings_text} {name}!", mimetype='text/plain')
